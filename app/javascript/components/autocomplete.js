a150460ef3ebd0fbc3804ce9fec4ac6f70d00894
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-27 22:17:16 +0200

    Search form jS

---
+++ app/javascript/components/autocomplete.js
@@ -0,0 +1,23 @@
+
+
+const autocompleteSearch = function() {
+  const ingredients = JSON.parse(document.getElementById('search-data').dataset.ingredients)
+  const searchInput = document.getElementById('q');
+
+  if (ingredients && searchInput) {
+    new autocomplete({
+      selector: searchInput,
+      minChars: 1,
+      source: function(term, suggest){
+          term = term.toLowerCase();
+          const choices = ingredients;
+          const matches = [];
+          for (let i = 0; i < choices.length; i++)
+              if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+          suggest(matches);
+      },
+    });
+  }
+};
+
+
