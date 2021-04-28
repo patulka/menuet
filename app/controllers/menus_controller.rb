e062a3dd31ef23620ca155ac5aaf4ad63282c450
Patricie Knopova <p.knopova@gmail.com>
2021-04-28 23:47:37 +0200

    multi ingredient search

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,15 +2,17 @@ class MenusController < ApplicationController
   skip_before_action :authenticate_user!
 
   def weekly_menu
-    if params[:qq].present?
+    query_list = ([params[:q]] + params[:qq].split(',')).reject { |q| q == "" }
+    menus_match = []
+    unless query_list.empty?
+      sql_like = query_list.map { |q| "ingredients.name LIKE \'%#{q}\'"}.join(' OR ')
       menus_match = RecipeIngredient.joins(:ingredient)
-                .where("ingredients.name LIKE \'%#{params[:qq]}\'")
+                .where(sql_like)
                 .joins(:recipe).all.sample(7).map { |x| x.recipe }
-      menus_rand = Recipe.all.sample(7 - menus_match.count)
-      @menus = menus_match + menus_rand
-    else
-      @menus = Recipe.all.sample(7)
     end
+    menus_rand = Recipe.all.sample(7 - menus_match.count)
+    @menus = menus_match + menus_rand
+
     @counter = 0
   end
 end
