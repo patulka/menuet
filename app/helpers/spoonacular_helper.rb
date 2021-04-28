f18c95a8ae4117e2d9d329b6d2a1b5a788cd142d
Patricie Knopová <77804308+patulka@users.noreply.github.com>
2021-04-28 09:44:21 +0200

    Update spoonacular_helper.rb

--- app/helpers/spoonacular_helper.rb
+++ app/helpers/spoonacular_helper.rb
@@ -1,32 +1,34 @@
-require 'open-uri'
-require 'json'
+# # Used to get random recipes from Spoonacular API
 
-module SpoonacularHelper
-  def self.get_random(number)
-    api_key = ENV['SPOONACULAR_API_KEY']
-    random_url = "https://api.spoonacular.com/recipes/random?number=#{number}"
-    url = random_url + "&apiKey=#{api_key}"
+# require 'open-uri'
+# require 'json'
 
-    recipes_json = JSON.parse(URI.open(url).read)
-    recipes = recipes_json["recipes"]
+# module SpoonacularHelper
+#   def self.get_random(number)
+#     api_key = ENV['SPOONACULAR_API_KEY']
+#     random_url = "https://api.spoonacular.com/recipes/random?number=#{number}"
+#     url = random_url + "&apiKey=#{api_key}"
 
-    recipes.map do |recipe|
-      next if Recipe.where("url = '#{recipe['sourceUrl']}'").exists?
+#     recipes_json = JSON.parse(URI.open(url).read)
+#     recipes = recipes_json["recipes"]
 
-      ingredients = ""
-      recipe["extendedIngredients"].each do |ingredient|
-        ingredients += "#{ingredient["name"]}\n"
-      end
+#     recipes.map do |recipe|
+#       next if Recipe.where("url = '#{recipe['sourceUrl']}'").exists?
 
-      Recipe.create(
-        title: recipe["title"],
-        author: recipe["creditsText"],
-        time: recipe["readyInMinutes"].to_i,
-        img_url: recipe["image"],
-        url: recipe["sourceUrl"],
-        ingredients: ingredients,
-        instructions: recipe["instructions"]
-      )
-    end.select { |recipe| not recipe.nil? }
-  end
-end
+#       ingredients = ""
+#       recipe["extendedIngredients"].each do |ingredient|
+#         ingredients += "#{ingredient["name"]}\n"
+#       end
+
+#       Recipe.create(
+#         title: recipe["title"],
+#         author: recipe["creditsText"],
+#         time: recipe["readyInMinutes"].to_i,
+#         img_url: recipe["image"],
+#         url: recipe["sourceUrl"],
+#         ingredients: ingredients,
+#         instructions: recipe["instructions"]
+#       )
+#     end.select { |recipe| not recipe.nil? }
+#   end
+# end
