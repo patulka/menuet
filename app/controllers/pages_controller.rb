f8539719e410009a3070d402203b38fd54257410
Patricie Knopova <p.knopova@gmail.com>
2021-04-30 12:03:46 +0200

    faq link works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :faq ]
 
   def home
     @ingredients = Ingredient.all.map { |ingredient| ingredient['name'] }
