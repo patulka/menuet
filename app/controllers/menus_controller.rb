79ffbacc6c64b9a3dd575b9e3d522a64e5cca476
Sarka Svobodna <sarka.svobodna@gmail.com>
2021-05-04 15:23:14 +0200

    able to exchange one recipe for text (not for html.erb)

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -9,20 +9,30 @@ class MenusController < ApplicationController
       # sql query to find all recipes which has at least one of the searched ingredients
       sql_like = query_list.map { |q| "ingredients.name LIKE \'%#{q}\'"}.join(' OR ')
       # set of recipes with searched ingredients
+      # menus_match = sample_from_db(RecipeIngredient.joins(:ingredient)
+      #                 .where(sql_like)
+      #                 .joins(:recipe), 7)
+      #               .map { |x| x.recipe } # selecting just recipe from join table
+      # # increased number of fetched recipes to 21 to be able to use shuffle for every recipe
       menus_match = sample_from_db(RecipeIngredient.joins(:ingredient)
                       .where(sql_like)
-                      .joins(:recipe), 7)
+                      .joins(:recipe), 21)
                     .map { |x| x.recipe } # selecting just recipe from join table
     end
 
     # set of recipes without searched ingredients
-    menus_rand = sample_from_db(Recipe, 7 - menus_match.count)
+    # menus_rand = sample_from_db(Recipe, 7 - menus_match.count)
+    # # increased number of fetched recipes to 21 to be able to use shuffle for every recipe
+    menus_rand = sample_from_db(Recipe, 21 - menus_match.count)
     # all 7 recipes together
     @menus = menus_match + menus_rand
 
     # saving the users queries, to be able to use them for "Give me different weekly menu"
     @params_q = params[:q]
     @params_qq = params[:qq]
+
+    # random recipe from @menus (but not the first 7, which are shown as the first option), to be given after clicking on shuffling button
+    @shuffled_recipe = @menus[7, 14].sample
 
     @counter = 0
   end
