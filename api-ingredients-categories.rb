77b69360a2f9af8947701740ade9a75a308206bf
Patricie Knopova <p.knopova@gmail.com>
2021-05-05 15:19:01 +0200

    prep for saving jsons with info

--- api-ingredients-categories.rb
+++ api-ingredients-categories.rb
@@ -6,14 +6,14 @@ num_saved = 0
 not_found = 0
 
 $API_KEYS = [
-  ENV[SPOONACULAR_API_KEY_1],
-  ENV[SPOONACULAR_API_KEY_2],
-  ENV[SPOONACULAR_API_KEY_3],
-  ENV[SPOONACULAR_API_KEY_4],
-  ENV[SPOONACULAR_API_KEY_5],
-  ENV[SPOONACULAR_API_KEY_6],
-  ENV[SPOONACULAR_API_KEY_7],
-  ENV[SPOONACULAR_API_KEY_8]
+  ENV["SPOONACULAR_API_KEY_1"],
+  ENV["SPOONACULAR_API_KEY_2"],
+  ENV["SPOONACULAR_API_KEY_3"],
+  ENV["SPOONACULAR_API_KEY_4"],
+  ENV["SPOONACULAR_API_KEY_5"],
+  ENV["SPOONACULAR_API_KEY_6"],
+  ENV["SPOONACULAR_API_KEY_7"],
+  ENV["SPOONACULAR_API_KEY_8"]
 ]
 
 # sets up new API key if the current one reached limit
@@ -28,23 +28,20 @@ def get_next_api_key
 end
 
 # looking up ingredients with api id
-ingredients = Ingredient.order("LENGTH(name) ASC").where("api_id IS NOT NULL").where("api_id IS NOT -1")
+api_ids = Ingredient.where("api_id > -1").distinct.pluck(:api_id)
 
 # turn off sql logging
 old_logger = ActiveRecord::Base.logger
 ActiveRecord::Base.logger = nil
 
 # main thing
-ingredients.each do |ingredient|
-  api_id = Ingredient.find(ingredient.id)
-  next unless api_id
-
+api_ids.each do |api_id|
   ing_info_url = "https://api.spoonacular.com/food/ingredients/#{api_id}/information?amount=1"
 
   while true
     # trying current API key and calling API
     begin
-      info_url = ing_info_url + "&apiKey=#{api_key}"
+      info_url = ing_info_url + "&apiKey=#{$api_key}"
       info = JSON.parse(URI.open(info_url).read)
     # catching if API key doesn't work
     rescue => error
@@ -56,40 +53,45 @@ ingredients.each do |ingredient|
       break
     end
   end
-#
-#   aisle = info["aisle"]
-#   category = info["categoryPath"]
 
   # just for terminal messages
   num_api_calls += 1
 
-  ok = false
-  ids.each do |result|
-    # dealing with special charachters, finding if found ingredient by API is in our DB
-    ingr = Ingredient.where(ActiveRecord::Base::sanitize_sql_array(['name = ?', result["name"].downcase]))[0]
-    next unless ingr
 
-    next if ingr.api_id
-
-    ok = true if ingr.id == ingredient.id
-
-    # setting up api_id and img_url
-    ingr.api_id = result["id"]
-    ingr.img_url_ingr = "https://spoonacular.com/cdn/ingredients_100x100/#{result["image"]}"
-    ingr.save
+  File.open("ingr_info_jsons/#{info["id"]}.json", 'w') do |file|
+    file.write(JSON.generate(info))
     num_saved += 1
   end
 
-  # ingredient not in our DB, put set -1 as api_id and show error message in terminal
-  unless ok
-    ingredient.api_id = -1
-    ingredient.save
-    not_found += 1
-    puts "WARINING: Ingredient #{ingredient.name} not found in API."
-  end
+  puts "saved: #{num_saved}, calls: #{num_api_calls}"
 
-  # showing results of API call
-  puts "saved: #{num_saved}, calls: #{num_api_calls}, not found: #{not_found}"
+  # ok = false
+  # ids.each do |result|
+  #   # dealing with special charachters, finding if found ingredient by API is in our DB
+  #   ingr = Ingredient.where(ActiveRecord::Base::sanitize_sql_array(['name = ?', result["name"].downcase]))[0]
+  #   next unless ingr
+
+  #   next if ingr.api_id
+
+  #   ok = true if ingr.id == ingredient.id
+
+  #   # setting up api_id and img_url
+  #   ingr.api_id = result["id"]
+  #   ingr.img_url_ingr = "https://spoonacular.com/cdn/ingredients_100x100/#{result["image"]}"
+  #   ingr.save
+  #   num_saved += 1
+  # end
+
+  # # ingredient not in our DB, put set -1 as api_id and show error message in terminal
+  # unless ok
+  #   ingredient.api_id = -1
+  #   ingredient.save
+  #   not_found += 1
+  #   puts "WARINING: Ingredient #{ingredient.name} not found in API."
+  # end
+
+  # # showing results of API call
+  # puts "saved: #{num_saved}, calls: #{num_api_calls}, not found: #{not_found}"
 end
 
 ActiveRecord::Base.logger = old_logger # turn logging back on
