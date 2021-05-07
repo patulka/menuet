2e60dcde28ac8c3916a2a4a78727bc35f37accdc
Sarka Svobodna <sarka.svobodna@gmail.com>
2021-05-07 11:17:34 +0200

    highlight of navbar centered -- changes style of nav-link, not the nav-item as before

--- app/javascript/components/active_navbar.js
+++ app/javascript/components/active_navbar.js
@@ -1,31 +1,3 @@
-// const activeNavbar = () => {
-
-//   const currentLocation = location.pathname.substr(1) // returns ending of the url
-
-
-//     const element = document.querySelector(`.`+`${currentLocation}`);
-//     console.log(element);
-//     console.log(element.classList);
-
-//     console.log(currentLocation);
-
-//     if (element) {
-
-//       console.log(element.classList);
-//       element.classList.add("current-page");
-//     } else {
-//       const elementHome = document.querySelector(`.`+`home`);
-//       if (elementHome) {
-//         console.log(elementHome.classList);
-//         elementHome.classList.add("current-page");
-//       }
-//     }
-
-
-// }
-
-
-
 const activeNavbar = () => {
 
   const currentLocation = location.pathname.substr(1) // returns ending of the url
@@ -44,16 +16,5 @@ const activeNavbar = () => {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 export { activeNavbar };
